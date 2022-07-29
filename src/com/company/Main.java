cf27c7629b26a2e6ef4983760368b127da9c0271
Munarbek74 <munarbekshorokhov@gmail.com>
2022-07-29 20:22:19 +0600

    1 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,6 @@ public class Main {
         name("Алтынай", "Айсулуу", "Наргиза", "Айпери", "Айнурв", "Бермет");
     }
 
-    static void name(String ... varags) {
-
-        for (String varag : varags) {
-            System.out.println("Я тебя люблю " + varag);
-        }
-
-        }
 
     }
 
