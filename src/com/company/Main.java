b6e556005fac162e5078e62f67492e9ee35923fa
Munarbek74 <munarbekshorokhov@gmail.com>
2022-07-30 11:47:33 +0600

    1 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,26 @@
 package com.company;
 
+import org.jetbrains.annotations.Contract;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        name("Алтынай", "Айсулуу", "Наргиза", "Айпери", "Айнурв", "Бермет");
+        boolean result = contains(86, 12, 25, 64, 35, 86, 5, 26, 54, 3, 6);
+        System.out.println(result);
     }
 
-
+    static boolean contains(int a, int... varargs) {
+    int k = 0;
+        for ( int d : varargs) {
+            if (d==a) {
+            k=d;
+                if (k>0) {return true;}
+            }
+        }
+            return false;
     }
+
+}
+
 
